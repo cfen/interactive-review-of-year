45ac069e21c11244cb7d9898194da3f00dac3ab9
cfen <chris.fenn@guardian.co.uk>
2015-12-02 17:47:14 +0000

    Tweaks

--- build/boot.js
+++ build/boot.js
@@ -1,9 +1,9 @@
-define(['https://interactive.guim.co.uk/2015/10/review-of-year/js/main.d93452e1.js'], function(app) {
+define(['http://localhost:8000/js/main.js'], function(app) {
     var css = document.createElement('link');
     
 	    css.type = 'text/css';
 	    css.rel = 'stylesheet';
-	    css.href = 'https://interactive.guim.co.uk/2015/10/review-of-year/css/main.a929f66d.css';
+	    css.href = 'http://localhost:8000/css/main.css';
 	    var head = document.head || document.getElementsByTagName('head')[0];
 	    head.appendChild(css);
     
