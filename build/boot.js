883a1c6f7f22fac59ebab05bd8ea7f3712ec0a38
cfen <chris.fenn@guardian.co.uk>
2015-12-03 12:01:38 +0000

    Margin top removed from figure.element css

--- build/boot.js
+++ build/boot.js
@@ -1,9 +1,9 @@
-define(['http://localhost:8000/js/main.js'], function(app) {
+define(['https://interactive.guim.co.uk/2015/10/review-of-year/js/main.13b050c8.js'], function(app) {
     var css = document.createElement('link');
     
 	    css.type = 'text/css';
 	    css.rel = 'stylesheet';
-	    css.href = 'http://localhost:8000/css/main.css';
+	    css.href = 'https://interactive.guim.co.uk/2015/10/review-of-year/css/main.53f972d7.css';
 	    var head = document.head || document.getElementsByTagName('head')[0];
 	    head.appendChild(css);
     
